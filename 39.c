19ed2b312894e3de46fcebc53c79c5a8d63a4045
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-02-11 09:04:50 +0530

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+#include<string.h>
+void main()
+{
+    int a[10],i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>c)
+            {
+                c=a[i];
+            }
+    }
+        printf("%d",c);
+    getch();
+}
