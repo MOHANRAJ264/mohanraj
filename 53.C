ed680d7d0d8e098653be3434f2f6f54aa5e486c7
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-02-11 13:09:12 +0530

    Create 53.C

---
+++ 53.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+    int a,sum=0,i=0,rem;
+    scanf("%d",&a);
+    i=0;
+    while(a)
+    {
+        rem=a%10;
+        a=a/10;
+       sum=sum+rem;
+        i++;
+    }
+  printf("%d",sum);
+    getch();
+}
