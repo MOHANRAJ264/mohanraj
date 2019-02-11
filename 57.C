cdccd586cda23f3bc7751d6c3e8303c28d373d61
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-02-11 13:13:03 +0530

    Create 57.C

---
+++ 57.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    int a[10],i,n,k=0,m;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==m)
+            k++;
+    }
+    printf("%d",k);
+    getch();
+}
