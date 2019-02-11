de8c5a6950ebe0813a281d1098ca9b507af7f8b8
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-02-11 13:29:44 +0530

    Create 72.C

---
+++ 72.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+char a[100];
+int i,k=0;
+gets(a);
+//scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+ if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+    {
+        k++ ;
+    }
+}
+if(k>0)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+getch();
+}
