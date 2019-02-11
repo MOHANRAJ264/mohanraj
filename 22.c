556135a8903f2a4f4eb54adee6d453ab03877c3a
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-02-11 09:08:58 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+    int a[10],i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>c)
+            {
+                c=a[i];
+            }
+    }
+        printf("%d",c);
+    getch();
+}
