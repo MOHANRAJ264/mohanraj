15691a5f31948c31ef2d304df8a3fa5725e7b81b
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-02-21 09:06:18 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,p,i,f=0;
+clrscr();
+scanf("%d%d",&a,&b);
+p=a*b;
+for(i=1;i<p;i++)
+{
+if(p==i*i)
+f=1;
+}
+if(f==1)
+printf("yes");
+else
+printf("no");
+
+}
